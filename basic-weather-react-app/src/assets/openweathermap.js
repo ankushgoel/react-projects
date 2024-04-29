3939b65dbc09b481a4228d3fbfe9bf35b14e1db8
Ankush Goel <goelankush1312@gmail.com>
2024-04-29 23:58:53 +0530

    react-basic-weather-app logic complete

---
+++ basic-weather-react-app/src/assets/openweathermap.js
@@ -0,0 +1,3 @@
+// const URLCity = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+// const URLLatLong = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+export const APIKey = "YOUR_API_KEY"
--- basic-weather-react-app/src/components/WeatherCard.jsx
+++ basic-weather-react-app/src/components/WeatherCard.jsx
@@ -1,36 +1,94 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaSearchLocation } from "react-icons/fa";
 import { BiWind } from "react-icons/bi";
+import { LuWaves } from "react-icons/lu";
+import { HiSun } from "react-icons/hi";
+import { PiCloudSunFill } from "react-icons/pi";
+
+import { APIKey } from '../assets/openweathermap';
 
 const WeatherCard = () => {
     const [city, setCity] = useState("")
     const [weatherData, setWeatherData] = useState(null)
     const [search, setSearch] = useState(false)
 
+    const fetchWeatherData = async (e) => {
+        e?.preventDefault();
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+            const data = await response.json();
+            console.log(data);
+            if (data.cod == 401) {
+                throw new Error(data.message);
+            }
+            setWeatherData(data);
+        } catch (error) {
+            console.log("Error while fetching weather data", error);
+            setWeatherData(null);
+        }
+        setSearch(true);
+    }
+
+    //ToDo - Add debouncing
+    useEffect(() => {
+        if (city && search) {
+            fetchWeatherData();
+        }
+    }, [city, search])
+
     return (
-        <div className='bg-gradient-to-r from-cyan-500 to-blue-500 mt-10 p-2 pt-4 w-[400px] h-[320px]'>
-            <div className='flex flex-row justify-between items-center bg-black rounded-2xl px-2'>
+        <div className='bg-indigo-600 bg-opacity-20 mt-8 p-2 pt-4 w-[500px] h-[470px] rounded'>
+            <form className='flex flex-row justify-between items-center bg-black rounded-2xl px-2'>
                 <input type="text"
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
                     className='rounded-2xl outline-none p-2'
                     placeholder='Enter City Name' />
-                <button className='bg-transparent outline-none'>
+                <button type='submit' onClick={fetchWeatherData} className='bg-transparent outline-none'>
                     <FaSearchLocation className='' />
                 </button>
-            </div>
+            </form>
             {search &&
                 (
                     <div>
                         {weatherData ? (
                             <div className="text-white">
-                                <div className="flex flex-col item-center text-5xl mt-5">
-                                    {(weatherData.main?.temp - 273.15)?.toFixed(2)}° C
+                                <div className='text-3xl mt-8'>{weatherData.name}, {weatherData.sys?.country}</div>
+                                <div className="flex flex-row justify-center text-5xl mt-5">
+                                    {(weatherData.main?.temp - 273.15)?.toFixed(2)}<span className='text-xl font-bold'>°C</span>
                                 </div>
-                                <div>{weatherData.name}</div>
 
+                                <div className="weather-info flex flex-row justify-around mt-10 text-xl px-4">
+                                    <div className='flex items-center gap-x-3 leading-tight'><BiWind />
+                                        <div>
+                                            <p>{(weatherData.wind?.speed * 3.6)?.toFixed(2)} km/h</p>
+                                            <p>Wind Speed</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex items-center gap-x-3 leading-tight'><LuWaves />
+                                        <div>
+                                            <p>{(weatherData.main?.humidity)?.toFixed(2)} %</p>
+                                            <p>Humidity</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="weather-info flex flex-row justify-around mt-14 text-xl px-4">
+                                    <div className='flex items-center gap-x-3 leading-tight'><HiSun />
+                                        <div>
+                                            <p>{new Date(weatherData.sys?.sunrise * 1000)?.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
+                                            <p>Sunrise</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex items-center gap-x-3 leading-tight'><PiCloudSunFill />
+                                        <div>
+                                            <p>{new Date(weatherData.sys?.sunset * 1000)?.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
+                                            <p>Sunset</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* //ToDo - Add Min and Max temp. */}
                             </div>
-                        ) : (<div> Data Not Found</div>)}
+                        ) : (<div className='mt-16 pt-5 font-bold text-3xl'> Data Not Found</div>)}
                     </div>
                 )
             }
