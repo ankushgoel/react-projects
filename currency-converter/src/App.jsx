629840699a04a9710fa78c43bfe3e83232459e2a
Ankush Goel <goelankush1312@gmail.com>
2024-05-05 23:48:58 +0530

    currency converter design & logic complete

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
+
 import './App.css'
 import Currency from './Currency'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <div>
+      <div className='mb-'>
         <h1>Currency Converter</h1>
       </div>
       <Currency />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </div>
   )
 }
--- currency-converter/src/Currency.jsx
+++ currency-converter/src/Currency.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const URL = 'https://v6.exchangerate-api.com/v6/{YOUR-API-KEY}/latest/USD'
 const Currency = () => {
 
-    const [amount1, setAmount1] = useState('')
+    const [amount1, setAmount1] = useState('1')
     const [amount2, setAmount2] = useState('')
     const [currency1, setCurrency1] = useState('USD')
     const [currency2, setCurrency2] = useState('INR')
@@ -14,12 +14,12 @@ const Currency = () => {
         try {
             const res = await fetch(URL)
             if (!res.ok) {
-                throw new Error('Failed')
+                throw new Error('Failed to fetch rates!')
             }
             const data = await res.json()
             console.log(res)
             console.log(data)
-            setExchangeRates(data.conversion_rates)
+            setExchangeRates(data.conversion_rates || {})
         } catch (error) {
             console.log('Error : ' + error);
         }
@@ -36,8 +36,44 @@ const Currency = () => {
         return (amount * rate).toFixed(2);
     }
 
+    useEffect(() => {
+        if (exchangeRates[currency1] && exchangeRates[currency2]) {
+            const convertedAmount = convertCurrency(amount1, currency1, currency2)
+            setAmount2(convertedAmount)
+        }
+    }, [amount1, currency1, currency2, exchangeRates])
+
     return (
-        <div>Currency</div>
+        <div className='mt-32'>
+            <form className='flex flex-row gap-x-12 items-center justify-evenly'>
+                <div className='border-2 border-white rounded-lg p-1 bg-white'>
+                    <select name="fromCurrency" id="" className='p-2 rounded-lg'
+                        value={currency1} onChange={(e) => setCurrency1(e.target.value)}>
+                        {Object.keys(exchangeRates).map((curr) => (
+                            <option key={curr} value={curr}>
+                                {curr}
+                            </option>
+                        ))}
+                    </select>
+                    <input type="number" value={amount1} onChange={(e) => setAmount1(e.target.value)}
+                        className='text-black bg-white p-2 focus:outline-none' />
+                </div>
+                <p>to</p>
+                <div className='border-2 border-white rounded-lg p-1 bg-white'>
+                    <select name="fromCurrency" id="" className='p-2 rounded-lg'
+                        value={currency2} onChange={(e) => setCurrency2(e.target.value)}>
+                        {Object.keys(exchangeRates).map((curr) => (
+                            <option key={curr} value={curr}>
+                                {curr}
+                            </option>
+                        ))}
+                    </select>
+                    {/* <input type="number" className='text-black bg-white p-2 focus:outline-none' /> */}
+                    <input type="number" value={amount2} onChange={(e) => setAmount2(e.target.value)}
+                        className='text-black bg-white ml-3 border-b-2 border-blue-700 outline-none' />
+                </div>
+            </form>
+        </div>
     )
 }
 
